42f2ffed8c6cf97e5ab11f08fe2f05c1d4beffd8
Lim Jun Kiat <limjunkiat5@gmail.com>
2025-03-20 14:46:04 +0800

    reverted db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
-require("dotenv").config();
 const { createClient } = require("@supabase/supabase-js");
-const https = require("https");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
 module.exports = supabase;
