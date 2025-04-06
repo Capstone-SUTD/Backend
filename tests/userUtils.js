bc8e8c3b223b3e413ecd46edbd800d20e39c4365
Rishika Banka <rishika0810banka@gmail.com>
2025-04-06 21:17:02 +0800

    new branch

--- tests/userUtils.js
+++
@@ -1,6 +0,0 @@
-async function getUserName(userid) {
-    const { data, error } = await supabase.from("users").select("username").eq("userid", userid);
-    return data[0]["username"];
-}
-
-module.exports = getUserName;
